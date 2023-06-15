ee3cea6c5f061bc7dbd0942390ff8efd608c7d9f
pewiebe <samuelirmaoisaias@hotmail.com>
2023-06-15 16:26:58 -0300

    a

---
+++ SQLQuery3.sql
@@ -0,0 +1,9 @@
+USE WebAppBloom
+  INSERT INTO Competencias(ColunaBloom,LinhaBloom)VALUES('Memorizar','Listar')
+  INSERT INTO Competencias(ColunaBloom,LinhaBloom)VALUES('Compreender','Esquematizar')
+  INSERT INTO Competencias(ColunaBloom,LinhaBloom)VALUES('Aplicar','Utilizar')
+  INSERT INTO Competencias(ColunaBloom,LinhaBloom)VALUES('Analisar','Resolver')
+  INSERT INTO Competencias(ColunaBloom,LinhaBloom)VALUES('Avaliar','Defender')
+  INSERT INTO Competencias(ColunaBloom,LinhaBloom)VALUES('Criar','Elaborar')
+
+  SELECT * FROM Competencias
